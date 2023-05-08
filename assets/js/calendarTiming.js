a52f40ebd83f7e2d405fcb9a13de0cec386acc4b
Anthony Villalba <anthonyvillalba@Anthonys-MacBook-Air.local>
2023-05-07 20:05:57 -0700

    adding date dynamically

--- assets/js/calendarTiming.js
+++ assets/js/calendarTiming.js
@@ -1,5 +1,7 @@
-const startDate = new Date('Apr 21, 2023')
+const startDate = new Date('Apr 21, 2023').toLocaleDateString()
 const date = new Date()
+const calendar = document.getElementById('calendar')
+console.log(startDate.toLocaleString())
+console.log(date.toLocaleDateString())
 
-console.log(date, startDate)
-
+calendar.append(`${startDate}`)
