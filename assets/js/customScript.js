74e878038785dd16b4725a36fb368a1347b1a05f
anthony villalba <anthony.villalba@thelastmile.org>
2023-05-11 23:11:32 -0700

    adding a comment

--- assets/js/customScript.js
+++ assets/js/customScript.js
@@ -8,3 +8,5 @@ signIn = (e) => {
     let user_name
     user_name === String ? user_name = $('#name').val().trim(): alert('type in a real name please') 
 }
+
+console.log('I promise to code double time tomorrow. I lost track of time and left little room to code today')
